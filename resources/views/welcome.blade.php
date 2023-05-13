fcf803acd9f8ea66b3f4cbcbb272c6b77badeaf0
alvianfazlur <m.alvianfazlurfd16@gmail.com>
2023-05-13 23:29:58 +0700

    sidebar + 1 marker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,23 +25,69 @@
 		}
 	</style>
 
+        <!-- Vendor CSS -->
+        <link rel="stylesheet" href="/lib/remixicon/fonts/remixicon.css" />
+        <link rel="stylesheet" href="/lib/jqvmap/jqvmap.min.css" />
+    
+        <!-- Template CSS -->
+        <link rel="stylesheet" href="/assets/css/style.min.css" />
 	
 </head>
 <body>
 
+    <div class="sidebar">
+        <div class="sidebar-header">
+          <a href="../" class="sidebar-logo">Kelompok 9 Hore</a>
+        </div>
+        <!-- sidebar-header -->
+        <div id="sidebarMenu" class="sidebar-body">
+          <div class="nav-group show">
+            <a href="#" class="nav-label">Dashboard</a>
+            <ul class="nav nav-sidebar">
+              <li class="nav-item">
+                <a href="../dashboard/finance.html" class="nav-link"><i class="ri-map-pin-line"></i> <span>Map</span></a>
+              </li>
+              <li class="nav-item">
+                <a href="../dashboard/events.html" class="nav-link"><i class="ri-database-line"></i> <span>Database</span></a>
+              </li>
+              <li class="nav-item">
+                <a href="../dashboard/sales.html" class="nav-link active"><i class="ri-contacts-line"></i> <span>Contact Us</span></a>
+              </li>
+            </ul>
+          </div>
 
-<div id="map" style="width: 600px; height: 400px;"></div>
+        </div>
+        <!-- sidebar-body -->
+        <!-- sidebar-footer -->
+      </div>
+      <!-- sidebar -->
+  
+      <div class="header-main px-3 px-lg-4">
+        <a id="menuSidebar" href="#" class="menu-link me-3 me-lg-4"><i class="ri-menu-2-fill"></i></a>
+  
+        <div class="form-search me-auto">
+          <input type="text" class="form-control" placeholder="Search" />
+          <i class="ri-search-line"></i>
+        </div>
+        <!-- form-search -->
+  
 
+      </div>
+      <!-- header-main -->
+
+      <div class="main main-app p-3 p-lg-4">
+        <div id="map" style="width: 75%; height: 400px;"></div>
+      </div>
 <script src="https://cdn.jsdelivr.net/leaflet/1.3.1/leaflet.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet-ajax/2.1.0/leaflet.ajax.min.js"></script>
 <script src="/dataset.js"></script>
 
 <script>
-
-	const map = L.map('map').setView([-7.6440623,112.7734131], 8);
+	
+    const map = L.map('map').setView([-7.6440623,112.7734131], 8);
 
 	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
+		maxZoom: 18,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
 
@@ -71,5 +116,20 @@
 L.geoJson(geoJsonData, {style: style}).addTo(map);
 </script>
 
+
+<script src="/lib/jquery/jquery.min.js"></script>
+<script src="/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/lib/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="/lib/jquery.flot/jquery.flot.js"></script>
+<script src="../lib/jquery.flot/jquery.flot.stack.js"></script>
+<script src="../lib/jquery.flot/jquery.flot.resize.js"></script>
+<script src="../lib/jquery.flot/jquery.flot.threshold.js"></script>
+<script src="../lib/chart.js/chart.min.js"></script>
+<script src="../lib/jqvmap/jquery.vmap.min.js"></script>
+<script src="../lib/jqvmap/maps/jquery.vmap.usa.js"></script>
+
+<script src="/assets/js/script.js"></script>
+<script src="/assets/js/db.data.js"></script>
+<script src="/assets/js/db.sales.js"></script>
 </body>
 
